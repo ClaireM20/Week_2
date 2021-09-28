809d39ea5fa3807f5b508a52c120a9ab6ff613b9
claireMgit config --global user.email clairegarside@icloud.com <clairegarside@Claires-MacBook-Air.local>
2021-09-28 09:29:09 +0100

    first commit week_2

---
+++ week_2js.js
@@ -0,0 +1,26 @@
+// FUNCTION TASK WEEK @ TASK 1
+ 
+ //function reverseString(str) {
+   // return str.split("").reverse().join("");
+   
+ //}
+ //reverseString ("my name is Claire");
+
+ 
+
+//ARRAYS
+let myArray = ["a", "b", "c", "d"];
+console.log(mayArray);
+console.log(myArray[2]);
+
+multi //dimensional array
+
+let myArray = [
+
+["a", "b", "c", "d"],
+["e", "f", "g", "h"],
+["i", "j", "k", "l"]
+
+];
+console.log(myArray[1][2]); 
+// this will output 'g'
