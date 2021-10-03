ecc9672045c9a3fb7767dd19d05aa3f6a18b50a7
claireMgit config --global user.email clairegarside@icloud.com <clairegarside@Claires-MacBook-Air.local>
2021-10-03 18:42:26 +0100

    Cloned website first version

---
+++ arrayswk2.js
@@ -0,0 +1,30 @@
+let coffeeOrder = [
+    
+    "alex-Cortado",
+    "Ben - Cortado",
+    "Charlie - Cortado"
+];
+//coffeeOrder[1] = "Ann - Vanilla latte";// replace Ben with Ann
+console.log(coffeeOrder);
+//coffeeOrder.push("Donna - espresso");
+//console.log(coffeeOrder);
+//coffeeOrder.pop();
+//console.log(coffeeOrder);
+
+//week 2 day 1
+//ARRAYS
+let myArray = ["a", "b", "c", "d"];
+console.log(myArray);
+console.log(myArray[2]);
+
+multi //dimensional array
+
+let myArray = [
+
+["a", "b", "c", "d"],
+["e", "f", "g", "h"],
+["i", "j", "k", "l"]
+
+];
+console.log(myArray[1][2]); 
+// this will output 'g'
